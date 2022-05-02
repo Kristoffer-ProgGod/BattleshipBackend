2eea8e2abc12b55f9cc4362f38d25e8236e3c89c
Kristoffer-ProgGod <Kristoffer.frederichsen@gmail.com>
2022-05-02 13:33:10 +0200

    Removed unused usings

--- Hubs/BattleshipHub.cs
+++ Hubs/BattleshipHub.cs
@@ -1,10 +1,5 @@
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
 using System.Numerics;
-using System.Text.Json;
-using System.Threading.Tasks;
 using gamelogic;
 using Microsoft.AspNetCore.SignalR;
 
